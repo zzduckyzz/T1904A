0b8f4ae9a52ab0c62aaabd0f350c2da5ded34eab
zzduckyzz <49919782+zzduckyzz@users.noreply.github.com>
2019-04-24 14:03:56 +0700

    AS1 24/4

---
+++ as1 so duong so am.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("tim so am va duong:\n");
+	scanf("%d", &a);
+	if(a >= 0)
+	{printf ("%d la so duong\n",a);
+	}
+	else
+	{
+	printf ("%d la so am\n",a);}
+	
+}
+ 
+
+
+
